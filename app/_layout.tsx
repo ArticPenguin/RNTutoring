16e72bd8b8cda391e4d88a1f3afcae142a6f18db
민욱 <fast1597@gmail.com>
2024-10-22 22:44:00 +0900

    Nativewind 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,11 +27,12 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" /> */}
+        <Stack.Screen name = "index"/>
       </Stack>
-    </ThemeProvider>
+    // </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import { Text, View } from 'react-native'
+
+// function App() {
+    
+// }
+
+// const App = (success, fail) => {
+//     // 통신 후 실행하고 싶은 함수
+//     const response = "success"
+//     if (true) {
+//         success(response)
+//     } else {
+//         fail()
+//     }
+// }
+
+
+// then((response) => {})
+
+// App(() => {
+//     console.log("성공")
+// }, () => {
+//     console.log("실패")
+// })
+
+
+const Index = () => {
+    return (
+      <View>
+        <Text className="text-blue-500">index</Text>
+      </View>
+    )
+}
+
+
+export default Index
